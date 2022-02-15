1773006e7b154eec38cb036312f4e59916f2839c
ficheran <nfichera35@gmail.com>
2022-02-15 15:43:32 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@ public class Hello
 {
 	public static void main(String argv[]) 
 	{
-		System.out.println("Hello, " + argv[0] + "!");
-
+		System.out.println("hello CSCI 4485, " + argv[0] + "!");
+		// Nick Fichera
 	}
 
 }
